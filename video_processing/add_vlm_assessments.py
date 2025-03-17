b0e0ccab53269affdb0d8410bfdbf91e87da50a0
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2025-03-17 13:52:01 +0530

    batch-inference-vlm-assessment

--- video_processing/add_vlm_assessments.py
+++ video_processing/add_vlm_assessments.py
@@ -21,6 +21,7 @@ parser.add_argument(
 )
 parser.add_argument("--use_fa2", action="store_true", help="If using Flash Attention 2.")
 parser.add_argument("--parquet-out-path", type=str, required=True)
+parser.add_argument("--chunk_size", type=int, default=4, help="Infer on batches of rows")
 args = parser.parse_args()
 path = pathlib.Path(args.path)
 
@@ -29,24 +30,32 @@ model_id = args.model_id
 use_fa2 = args.use_fa2
 parquet_path = pathlib.Path(args.parquet_path)
 parquet_out_path = pathlib.Path(args.parquet_out_path)
+chunk_size = args.chunk_size
 
 model, processor = load_vlm_model(model_id, use_fa2=use_fa2)
 df = pd.read_parquet(parquet_path)
 
 data = []
+messages_list = []
+answer_list = []
 with tqdm() as pbar:
-    for _, row in df.iterrows():
+    for idx, row in df.iterrows():
         pbar.set_description(row["file"])
         key_frames, first, mid, last = separate_key_frames_from_row(path, row)
         pbar.set_postfix_str(f"{len(key_frames)} key frames")
 
         messages = create_messages(key_frames, effect=effect)
-        answer = run_vlm(model, processor, messages)
-        answer = recover_json_from_output(answer)
 
-        row = {f"{effect}": answer[effect], "confidence": float(answer["confidence"])}
-        data.append(row)
-        pbar.update()
+        if idx % chunk_size == 0 and idx != 0:
+            answer_list = run_vlm(model, processor, messages_list)            
+            answer_list = [recover_json_from_output(answer) for answer in answer_list]
+
+            row = [{f"{effect}": answer[effect], "confidence": float(answer["confidence"])} for answer in answer_list]
+            data.extend(row)
+            pbar.update()
+            messages_list = list()
+        else:
+            messages_list.append(messages)
 
 shot_categorized_df = pd.DataFrame(data)
 
